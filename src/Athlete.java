0153ca9098f740ce0a9ce18b6b1add1cd468b530
league <league@wts14-2.attlocal.net>
2019-01-05 12:56:23 -0800

    Athlete

---
+++ src/Athlete.java
@@ -0,0 +1,24 @@
+
+public class Athlete {
+
+     static int nextBibNumber;
+     static String raceLocation = "New York";
+     static String raceStartTime = "9.00am";
+
+     String name;
+     int speed;
+     int bibNumber;
+
+Athlete (String name, int speed){
+     this.name = name;
+     this.speed = speed;
+}
+
+public static void main(String[] args) {
+     //create two athletes     
+	
+	//print their names, bibNumbers, and the location of their race. }
+}
+
+
+}
